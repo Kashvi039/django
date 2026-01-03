c3ed37e17a507c2ac58fa3657c11fb9f07b124d5
kashvi dua <kasvidua39@gmail.com>
2026-01-03 14:38:55 +0530

    blogs app and blog model

--- blog/settings.py
+++ blog/settings.py
@@ -125,3 +125,9 @@ STATICFILES_DIRS = [
 ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#included manually
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from . models import category,Blog
+
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields={'slug' : ('title',)}
+    list_display= ('title','Category','author','status','is_featured')
+
 
 # Register your models here.
+admin.site.register(category)
+admin.site.register(Blog,BlogAdmin)
